ded5af0c946a018908a1f26f456b589894e7628b
kavlak34 <gokhankavlak24@gmail.com>
2021-01-01 17:26:13 +0300

    bottom nav bar imp1

--- app/src/main/java/com/example/dolares/ui/MainActivity.kt
+++ app/src/main/java/com/example/dolares/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.dolares.ui
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,12 +10,13 @@ import com.example.dolares.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-    val navController = findNavController(R.id.nav_graph)
+    val navController = findNavController(R.id.nav_host_fragment)
 
      val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
@@ -29,16 +31,16 @@ class MainActivity : AppCompatActivity() {
             // R.id.registerFragment -> {
             //        bottomNavigationView.visibility = View.GONE
                // }
-              R.id.coresFragment -> {
+              R.id.CoresFragment -> {
                     bottomNavigationView.visibility = View.VISIBLE
                 }
-                R.id.capsulesFragment -> {
+                R.id.CapsuleFragment -> {
                    bottomNavigationView.visibility = View.VISIBLE
                 }
                 R.id.settingsFragment -> {
                    bottomNavigationView.visibility = View.VISIBLE
                 }
-                    R.id.launchpadsFragment -> {
+                R.id.launchPadsFragment -> {
                         bottomNavigationView.visibility = View.VISIBLE
                           }
            }
